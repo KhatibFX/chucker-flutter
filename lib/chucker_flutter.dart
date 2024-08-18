b20b121925e70318e168e87e60d760e8856a3afa
Ahmed Elkhatib <5616640+KhatibFX@users.noreply.github.com>
2024-08-18 17:27:03 +0300

    Refactored implementation

--- lib/chucker_flutter.dart
+++ lib/chucker_flutter.dart
@@ -1,14 +1,14 @@
 ///An HTTP requests inspector inspired by Chucker Android
 library chucker_flutter;
 
+export 'src/helpers/i_storage_manager.dart';
+export 'src/helpers/isar_manager.dart';
+export 'src/helpers/shared_preferences_manager.dart';
 export 'src/helpers/status_code_map.dart';
 export 'src/http/chucker_http_client.dart';
 export 'src/interceptors/chopper_interceptor.dart';
 export 'src/interceptors/dio_interceptor.dart';
 export 'src/interceptors/http_logging_interceptor.dart';
+export 'src/models/api_response_db.dart';
+export 'src/models/settings_db.dart';
 export 'src/view/helper/chucker_ui_helper.dart' show ChuckerFlutter;
-export 'src/helpers/i_storage_manager.dart';
-export 'src/helpers/shared_preferences_manager.dart';
-export 'src/helpers/isar_manager.dart';
-
-
